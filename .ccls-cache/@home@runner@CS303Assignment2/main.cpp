3f64f5199f86ed514ee31c1f3eebc56ee2615208
voiidtyde <123600927+voiidtyde@users.noreply.github.com>
2023-10-06 16:50:51 +0000

    header fixes

--- .ccls-cache/@home@runner@CS303Assignment2/main.cpp
+++ .ccls-cache/@home@runner@CS303Assignment2/main.cpp
@@ -1,10 +1,19 @@
-#include "Single_Linked_List.h"
+#include "Employee.h"
 #include "Professional.h"
 #include "Nonprofessional.h"
 
 int main() {
+  //Creating an instance for each derived class (Professional and Nonprofessional)
   Professional tommy("Tommy", 1, 25000, 5);
   Nonprofessional jacob("Jacob", 2);
-  
-  
+
+  //Tests for Professional class derived functions
+  cout << "Professional Weekly Salary: " << tommy.calculateWeeklySalary() << endl;
+  cout << "Professional Health Care: " << tommy.calculateHealthCare() << endl;
+  cout << "Professional Vacation Days: " << tommy.calculateVacationDays();
+
+  //Tests for Nonprofessional class derived functions
+  cout << "Nonprofessional Weekly Salary: " << jacob.calculateWeeklySalary() << endl;
+  cout << "Nonprofessional Health Care: " << jacob.calculateHealthCare() << endl;
+  cout << "Nonprofessional Vacation Days: " << jacob.calculateVacationDays() << endl;
 }
--- Employee.h
+++ Employee.h
@@ -22,4 +22,4 @@ protected:
 };
 
 
-#endif
+#endif 
